86f0c564c714b3281a42cb6063fa14407d4d80eb
OJII3 <84656786+OJII3@users.noreply.github.com>
2022-12-26 21:56:50 +0900

    update

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -9,10 +9,7 @@ const HttpServer = {
 	server: createServer(app),
 
 	init() {
-		app.get('/', (_, res) => {
-			console.log('get /');
-			res.sendFile(`${__dirname}/pages/mqtt.html`);
-		});
+		app.use(express.static('src/pages'));
 
 		this.server.listen(port, () => {
 			console.log(`http server listening at http://localhost:${port}`);
