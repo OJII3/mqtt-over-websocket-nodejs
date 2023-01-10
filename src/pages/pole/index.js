ed9a3143306537bb7c977f7bb7b9587091bd8c01
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-10 18:11:55 +0900

    update

---
+++ src/pages/pole/index.js
@@ -0,0 +1,29 @@
+const log = document.querySelector('#log');
+const domain = document.querySelector('#domain');
+const port = document.querySelector('#port');
+const topic = document.querySelector('#topic');
+const metric = document.querySelector('#metric');
+
+let client;
+
+const connect = () => {
+  client = mqtt.connect(`ws://${domain.value}:${port.value}`);
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    log.innerHTML = `${new Date()}: ${message.toString()}\n${log.innerHTML}`;
+  });
+};
+const pub = () => {
+  client.publish(topic.value, metric.value);
+};
+const sub = () => {
+  client.subscribe(topic.value);
+};
+const end = () => {
+  client.end();
+};
+
+const connectButton = document.querySelector('#connect').addEventListener('click', connect);
+const subButton = document.querySelector('#sub').addEventListener('click', sub);
+const pubButton = document.querySelector('#pub').addEventListener('click', pub);
+const endButton = document.querySelector('#end').addEventListener('click', end);
